dd4b352af2770a606e1de8b04516bbdab234eee5
heybys <byn0174@gmail.com>
2024-08-19 01:44:25 +0900

    feat: update nav style

--- src/components/GlobalNavigation.tsx
+++ src/components/GlobalNavigation.tsx
@@ -6,26 +6,31 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import {Link} from "@radix-ui/react-navigation-menu";
+import {cva} from "class-variance-authority";
+
+const globalNavigationWrapperStyle = cva('flex justify-center');
 
 export function GlobalNavigation() {
   return (
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href={"/"}>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href={"/food"}>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Food
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <div className={globalNavigationWrapperStyle()}>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href={"/"}>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href={"/food"}>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Food
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
   )
 }
