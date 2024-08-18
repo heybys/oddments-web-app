85299e4fc5d307e7fe6bc8f20c8adbb5579c686c
heybys <byn0174@gmail.com>
2024-08-19 00:26:13 +0900

    init: set project with shadcn, tailwind

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import EnvironmentPlugin from 'vite-plugin-environment';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), EnvironmentPlugin('all'), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
